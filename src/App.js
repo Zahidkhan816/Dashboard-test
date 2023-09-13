4de5d1a62d51a41e550676ca1568712c485e6ddf
Zahidkhan816 <zahidkhan717816@gmail.com>
2023-09-13 08:59:05 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NewPage from './Compnents/NewPage';
+
+function App() {
+  return (
+    <div>
+    <NewPage/>
+    </div>
+  );
+}
+
+export default App;
