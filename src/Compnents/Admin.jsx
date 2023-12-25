3e9e6263ea2584cdc96521c0b555404eac807dfb
Zahidkhan816 <zahidkhan717816@gmail.com>
2023-12-25 23:17:42 +0500

    update admin

--- src/Compnents/Admin.jsx
+++ src/Compnents/Admin.jsx
@@ -1,5 +1,6 @@
-import React, { useState ,  } from 'react';
-
+import React, { useState, } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserTie, faUser } from '@fortawesome/free-solid-svg-icons';
 import {
   Layout,
   Card,
@@ -17,6 +18,7 @@ import {
   Input,
   Select,
 } from 'antd';
+import InputMask from 'react-input-mask';
 import './Admin.css';
 import {
   DashboardOutlined,
@@ -35,16 +37,16 @@ const { Meta } = Card;
 const { TabPane } = Tabs;
 const { Option } = Select;
 const cardsData = [
-  { icon: <DashboardOutlined />, title: 'Users ', value: 4002, avg: "16% vs 30Dni" },
-  { icon: <CarOutlined />, title: 'Obrót', value: 57820, avg: "26% vs 30Dni" },
-  { icon: <ShopOutlined />, title: 'Przecenione ', value: 12345, avg: "46% vs 30Dni" },
-  { icon: <SettingOutlined />, title: 'Total points ', value: 9876, avg: "56% vs 30Dni" },
+  // { icon: <DashboardOutlined />, title: 'Users ', value: 4002, avg: "16% vs 30Dni" },
+  // { icon: <CarOutlined />, title: 'Obrót', value: 57820, avg: "26% vs 30Dni" },
+  // { icon: <ShopOutlined />, title: 'Przecenione ', value: 12345, avg: "46% vs 30Dni" },
+  { icon: <SettingOutlined />, title: 'Total points Earned', value: 9876, avg: "56% vs 30Dni" },
 ];
 
 const Admin = () => {
-  const [activeTab, setActiveTab] = useState('active');
+  const [activeTab, setActiveTab] = useState('Active');
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [formData, setFormData] = useState({ name: '', email: '', phone: '' });
+  const [formData, setFormData] = useState({ name: '', email: '', phone: '', role: '' });
   const [form] = Form.useForm();
 
   const showModal = () => {
@@ -52,14 +54,15 @@ const Admin = () => {
   };
 
   const handleCancel = () => {
+    form.resetFields();
     setIsModalVisible(false);
   };
+
 
   const handleFormSubmit = () => {
     form
       .validateFields()
       .then((values) => {
-        // Reset the form and close the modal
         form.resetFields();
         setIsModalVisible(false);
       })
@@ -72,27 +75,38 @@ const Admin = () => {
     setActiveTab(key);
   };
 
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
 
-  const color = getRandomColor();
 
   const data = [
-    { key: '1', user: 'John Doe', points: 150, userId: 'ABC123', status: "Active" },
-    { key: '2', user: 'Jane Smith', points: 200, userId: 'XYZ456', status: "Inactive" },
-    { key: '3', user: 'zahid khan', points: 100, userId: "key41314", status: "Pending" }
+    { key: '1', user: 'John Doe', points: 150, userId: 'ABC123', role: 'admin', status: "Active" },
+    { key: '2', user: 'Jane Smith', points: 200, userId: 'XYZ456', role: 'member', status: "Inactive" },
+    { key: '3', user: 'zahid khan', points: 100, userId: "key41314", role: 'member', status: "Pending" },
+    { key: '4', user: 'Ali ahmad', points: 90, userId: "key42213131", role: 'member', status: "Pending" },
+    { key: '5', user: 'Hamza', points: 90, userId: "key4225242134", role: 'admin', status: "Active" },
+    { key: '6', user: 'naveed', points: 90, userId: "key473252134", role: 'member', status: "Active" }
   ];
 
   const columns = [
-    { title: 'Users', dataIndex: 'user', key: 'user' },
-    { title: 'User ID', dataIndex: 'userId', key: 'userId' },
-    { title: 'Points', dataIndex: 'points', key: 'points' },
+    { title: 'Users', dataIndex: 'user', key: 'user', role: 'role' },
+    { title: 'User ID', dataIndex: 'userId', key: 'userId', role: 'role' },
+    { title: 'Earned Points', dataIndex: 'points', key: 'points', role: 'role' },
+    {
+      title: 'Role',
+      dataIndex: 'role',
+      key: 'role',
+      render: (text) => {
+        if (text === 'admin') {
+          // Render FontAwesome icon for admin
+          return <><span>Admin</span> <FontAwesomeIcon icon={faUserTie} size='lg' /></>;
+        } else if (text === 'member') {
+          // Render FontAwesome icon for member
+          return <><span>Member</span> <FontAwesomeIcon icon={faUser} size='lg' /></> ;
+        } else {
+          
+          return text;
+        }
+      },
+    },
     {
       title: 'Status',
       dataIndex: 'status',
@@ -113,18 +127,19 @@ const Admin = () => {
           <Dropdown
             overlay={
               <Menu>
-                {record.status === 'pending' || record.status === 'inactive' ? (
-                  <>
-                    <Menu.Item key="1">Delete</Menu.Item>
-                    <Menu.Item key="2">Activate</Menu.Item>
-                  </>
-                ) : (
-                  <>
-                    <Menu.Item key="3">Delete</Menu.Item>
-                    <Menu.Item key="4">Suspend</Menu.Item>
-                  </>
-                )}
-              </Menu>
+              {record.status === 'pending' || record.status === 'inactive' ? (
+                <>
+                  <Menu.Item key="1">Delete</Menu.Item>
+                  <Menu.Item key="2">Activate</Menu.Item>
+                </>
+              ) : (
+                <>
+                  <Menu.Item key="3">Delete</Menu.Item>
+                  <Menu.Item key="4">Suspend</Menu.Item>
+                  {record.role === 'member' && <Menu.Item key="5">Make Admin</Menu.Item>}
+                </>
+              )}
+            </Menu>   
             }
             placement="bottomRight"
             trigger={['click']}
@@ -140,7 +155,7 @@ const Admin = () => {
     if (activeTab === 'Active') {
       return item.status === "Active";
     } else if (activeTab === 'Inactive') {
-      return item.status === "iInactive" || item.status === "Pending";
+      return item.status === "Inactive" || item.status === "Pending";
     }
     return true;
   });
@@ -169,15 +184,13 @@ const Admin = () => {
             </div>
             <div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap', padding: '16px' }}>
               {cardsData.map((card, index) => (
-                <Card key={index} style={{ width: '25%', height: "200px", marginBottom: '16px' }}>
+                <Card key={index} style={{ width: '27%', height: "200px", marginBottom: '16px' }}>
                   <Meta
                     avatar={card.icon}
-                    title={<span style={{ color: color }}>{card.title}</span>}
+                    title={<span style={{ color: "Black" }}>{card.title}</span>}
                     description={
                       <>
                         <h2>{`${card.value}k`}</h2>
-                        <br />
-                        <p>{`${card.avg}`}</p>
                       </>
                     }
                   />
@@ -195,12 +208,12 @@ const Admin = () => {
                     </Button>
                   }
                 >
-                  <TabPane tab={<span> Active</span>} key="Active">
+                  <TabPane tab={<span> Active</span>} key="Active" >
                     <div style={{ padding: '16px' }}>
                       <Table dataSource={filteredData} columns={columns} />
                     </div>
                   </TabPane>
-                  <TabPane tab={<span> Inactive</span>} key="inactive">
+                  <TabPane tab={<span> Inactive</span>} key="Inactive">
                     <div style={{ padding: '16px' }}>
                       <Table dataSource={filteredData} columns={columns} />
                     </div>
@@ -242,7 +255,15 @@ const Admin = () => {
                       name="phone"
                       rules={[{ required: true, message: 'Please enter your phone number' }]}
                     >
-                      <Input />
+                      <InputMask
+                        mask="(999) 999-9999"
+                        maskChar=""
+                        alwaysShowMask={false}
+                      >
+                        {(inputProps) => (
+                          <Input {...inputProps} placeholder="(999) 999-9999" />
+                        )}
+                      </InputMask>
                     </Form.Item>
                     <label htmlFor="Role">Role*</label>
                     <Form.Item
